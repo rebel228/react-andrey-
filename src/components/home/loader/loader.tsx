cc0366a4353e0b07f52abe9709339b77f83dceee
mindr17 <mindr17@yandex.ru>
2023-10-26 15:55:46 +0300

    refactor: loader

--- src/components/home/loader/loader.tsx
+++ src/components/home/loader/loader.tsx
@@ -2,26 +2,16 @@ import { Component } from 'react';
 
 import s from './loader.module.css';
 
-interface Props {}
-
-interface State {}
-
-export default class Loader extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      product: undefined,
-    };
-  }
-
+class Loader extends Component {
   render() {
     return (
       <div className={s.container}>
         <div className={s.spinnerContainer}>
-          <div className={s.loadingSpinner}>123</div>
+          <div className={s.loadingSpinner} />
         </div>
       </div>
     );
   }
 }
+
+export default Loader;
